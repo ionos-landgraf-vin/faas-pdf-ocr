2d6016124f22a78a88299f667e6a0097fc618124
Lars Gersmann <lars.gersmann@gmail.com>
2022-06-22 16:03:46 +0200

    fix: img-to-text test case fixed

--- tests/img-to-text/index.js
+++ tests/img-to-text/index.js
@@ -4,13 +4,9 @@ const process = require("process");
 const tesseract = require("tesseract.js");
 
 tesseract
-  .recognize("tests/img-to-text/img-to-text.png", "eng", {
+  .recognize("tests/img-to-text/img-to-text.png", "eng+deu", {
     logger: (m) => console.log(m),
   })
   .then(({ data: { text } }) => {
     console.log(text);
   });
-
-let i = 1
-console.log(i);
-console.log("hello lars test");
