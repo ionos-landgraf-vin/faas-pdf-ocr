e79f5aac7e7a4582f21b76f670bf58bdaf59b0f8
Vincent Landgraf <vincent.landgraf@ionos.com>
2022-06-22 14:22:39 +0200

    initial layout for the main handler

--- README.md
+++ README.md
@@ -15,3 +15,13 @@
 ## tests
 
 Create individual test cases in individual directories inside directory `tests`.
+
+## config
+
+Done via environnement variables:
+
+    IONOS_S3_KEY_ID="..."
+    IONOS_ACCESS_KEY="...."
+    IONOS_S3_REGION="eu-central-2"
+    IONOS_S3_ENDPOINT="s3-eu-central-1.ionoscloud.com"
+    IONOS_S3_BUCKET="..."
--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 #!/usr/bin/env nodejs
 
-console.log("hello faas");
+var pipeline = [
+    DownloadFileFromS3,
+    ConvertPDFtoImages,
+    ExtractTextFromImageAndConvertToPDF,
+    MergePagesToOnePDF,
+    UploadPDFToS3,
+];
+
+exports.handler = async function(event, context) {
+    var job = {
+        event: event, // original from request
+        context: context, // original from request
+        vars: {
+            localFilename: "", // location of the file on disk
+            s3Location: "", // s3 location of the file
+            s3client: null, // shared between upload/download
+            imageLocations: [], // locations of the images on disk
+            pdfLocations: [], // locations of the generated pdf files
+            finalPDFWithTextLocation: "", // location of the merged pdf with text
+            metadata: {},
+        },
+        env: process.env,
+    };
+
+    for(fn of pipeline) {
+        fn(job);
+    }
+
+    console.log(JSON.stringify(job.vars.metadata));
+};
