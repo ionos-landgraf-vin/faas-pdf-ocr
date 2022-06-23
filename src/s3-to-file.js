7e2a51f5fff1449b0823000e088001b58ffcfeda
Lars Gersmann <lars.gersmann@gmail.com>
2022-06-23 11:47:16 +0200

    fix: event json structure fixed

--- src/s3-to-file.js
+++ src/s3-to-file.js
@@ -1,15 +1,15 @@
 const path = require("path");
 const Ajv = require("ajv");
 const os = require("os");
-const fs = require('fs');
+const fs = require("fs");
 const schema = require("./s3-event-schema").schema;
 
 const ajv = new Ajv();
 const validate = ajv.compile(schema);
 
-exports.DownloadFileFromS3 = async function(job) {
+exports.DownloadFileFromS3 = async function (job) {
   // parse request
-  let requestBody = JSON.parse(job.event.body);
+  let requestBody = JSON.parse(job.event);
 
   // validate that the request is semantically correct
   const valid = validate(requestBody);
@@ -22,54 +22,68 @@ exports.DownloadFileFromS3 = async function(job) {
     // setup job variables
     job.vars.s3Bucket = record.s3.bucket.name;
     job.vars.s3Key = record.s3.object.key;
-    job.vars.localFilename = path.join(os.tmpdir(), record.s3.object.eTag + ".pdf");
+    job.vars.localFilename = path.join(
+      os.tmpdir(),
+      record.s3.object.eTag + ".pdf",
+    );
 
     // check if the file exists
     await keyExists(job);
 
     // download the file to localFilename
-    await downloadFile(job)
+    await downloadFile(job);
   }
-}
+};
 
 async function keyExists(job) {
   return new Promise((resolve, reject) => {
-    job.vars.s3Client.s3.headObject({
-      Bucket: job.vars.s3Bucket,
-      Key: job.vars.s3Key,
-    }, function(err, data) {
-      if (err) {
-        return reject(err);
-      }
-      
-      resolve();
-    });
-  })
+    job.vars.s3Client.s3.headObject(
+      {
+        Bucket: job.vars.s3Bucket,
+        Key: job.vars.s3Key,
+      },
+      function (err, data) {
+        if (err) {
+          return reject(err);
+        }
+
+        resolve();
+      },
+    );
+  });
 }
 
 async function downloadFile(job) {
-  console.log("Download ing", "s3://"+job.vars.s3Bucket+"/"+job.vars.s3Key, "to", job.vars.localFilename);
+  console.log(
+    "Download ing",
+    "s3://" + job.vars.s3Bucket + "/" + job.vars.s3Key,
+    "to",
+    job.vars.localFilename,
+  );
   return new Promise((resolve, reject) => {
-    job.vars.s3Client.s3.getObject({
-      Bucket: job.vars.s3Bucket,
-      Key: job.vars.s3Key,
-    }, function(err, data) {
-      if (err) {
-        console.log("unable to download:", err);
-        reject(err);
-        return
-      }
-
-      console.log("done downloading");
-      fs.writeFile(job.vars.localFilename, data.Body, err => {
+    job.vars.s3Client.s3.getObject(
+      {
+        Bucket: job.vars.s3Bucket,
+        Key: job.vars.s3Key,
+      },
+      function (err, data) {
         if (err) {
+          console.log("unable to download:", err);
           reject(err);
-          return
+          return;
         }
 
-        // file written successfully
-        resolve();
-      });
-    });
-  })
+        console.log("done downloading");
+        fs.writeFile(job.vars.localFilename, data.Body, (err) => {
+          if (err) {
+            reject(err);
+            return;
+          }
+
+          // file written successfully
+          resolve();
+        });
+      },
+    );
+  });
 }
--- tests/s3-to-file/index.js
+++ tests/s3-to-file/index.js
@@ -6,53 +6,52 @@ var DownloadFileFromS3 = require("../../src/s3-to-file").DownloadFileFromS3;
 async function TestDownloadFileFromS3() {
   try {
     await DownloadFileFromS3({
-      event:{
-        body: JSON.stringify({
-          "Records": [
-            {
-              "eventVersion": "2.0",
-              "eventSource": "aws:s3",
-              "awsRegion": "us-east-1",
-              "eventTime": "1970-01-01T00:00:00.123Z",
-              "eventName": "ObjectCreated:Put",
-              "userIdentity": {
-                "principalId": "EXAMPLE"
-              },
-              "requestParameters": {
-                "sourceIPAddress": "127.0.0.1"
-              },
-              "responseElements": {
-                "x-amz-request-id": "C3D13FE58DE4C810",
-                "x-amz-id-2": "FMyUVURIY8/IgAtTv8xRjskZQpcIZ9KG4V5Wp6S7S/JRWeUWerMUE5JgHvANOjpD"
-              },
-              "s3": {
-                "s3SchemaVersion": "1.0",
-                "configurationId": "testConfigRule",
-                "bucket": {
-                  "name": "faas-ocr-on-pdf",
-                  "ownerIdentity": {
-                    "principalId": "EXAMPLE"
-                  },
-                  "arn": "arn:aws:s3:::mybucket"
+      event: JSON.stringify({
+        Records: [
+          {
+            eventVersion: "2.0",
+            eventSource: "aws:s3",
+            awsRegion: "us-east-1",
+            eventTime: "1970-01-01T00:00:00.123Z",
+            eventName: "ObjectCreated:Put",
+            userIdentity: {
+              principalId: "EXAMPLE",
+            },
+            requestParameters: {
+              sourceIPAddress: "127.0.0.1",
+            },
+            responseElements: {
+              "x-amz-request-id": "C3D13FE58DE4C810",
+              "x-amz-id-2":
+                "FMyUVURIY8/IgAtTv8xRjskZQpcIZ9KG4V5Wp6S7S/JRWeUWerMUE5JgHvANOjpD",
+            },
+            s3: {
+              s3SchemaVersion: "1.0",
+              configurationId: "testConfigRule",
+              bucket: {
+                name: "faas-ocr-on-pdf",
+                ownerIdentity: {
+                  principalId: "EXAMPLE",
                 },
-                "object": {
-                  "key": "API Key.txt",
-                  "size": 1024,
-                  "versionId": "version",
-                  "eTag": "d41d8cd98f00b204e9800998ecf8427e",
-                  "sequencer": "Happy Sequencer"
-                }
-              }
-            }
-          ]
-        }),
-      },
+                arn: "arn:aws:s3:::mybucket",
+              },
+              object: {
+                key: "test1.pdf",
+                size: 1024,
+                versionId: "version",
+                eTag: "d41d8cd98f00b204e9800998ecf8427e",
+                sequencer: "Happy Sequencer",
+              },
+            },
+          },
+        ],
+      }),
       vars: {
         s3Client: s3client,
       },
-    })
+    });
   } catch (err) {
-    console.error("Downloading failed", err)
+    console.error("Downloading failed", err);
   }
 }
 
